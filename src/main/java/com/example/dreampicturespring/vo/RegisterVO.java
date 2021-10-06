c4cc3a231106e068ad085eeffe5e966be3254675
Kim Seonggyu <tjdrb200@naver.com>
2021-10-06 20:49:12 +0900

    red

--- src/main/java/com/example/dreampicturespring/vo/RegisterVO.java
+++ src/main/java/com/example/dreampicturespring/vo/RegisterVO.java
@@ -2,11 +2,10 @@ package com.example.dreampicturespring.vo;
 
 import lombok.Data;
 
-//hello
-//eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 @Data
 public class RegisterVO {
     private String email;
     private String pw;
     private String phone;
+
 }
