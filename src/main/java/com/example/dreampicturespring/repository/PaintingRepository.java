75bd3316d326d842af45640350ab2823f06401ef
joshua <iuttn123@gmail.com>
2021-10-13 19:48:07 +0900

    ..

--- src/main/java/com/example/dreampicturespring/repository/PaintingRepository.java
+++ src/main/java/com/example/dreampicturespring/repository/PaintingRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
 public interface PaintingRepository extends JpaRepository<Paintingtbl,Long> {
-    @Query(value = "select pname from Paintingtbl where Paintingtbl.no_membership = :id",nativeQuery = true)
-    List<String> findbyno_painiting(@Param(value = "id") int id);
+    @Query(value = "select membershiptbl.nickname,paintingtbl.pname,membershiptbl.img from paintingtbl, membershiptbl where paintingtbl.no_membership = membershiptbl.no_membership",nativeQuery = true)
+    List<String> findbyno_painiting();
 
 /*    @Query(value = "select * from paintingtbl where width<=500 or height<=200 or style='oils' or theme='still' = :id",nativeQuery = true)
     List<String>*/
