d967bda9f8ff3749a74c2a73cea4734e6cd0c021
Kim Seonggyu <tjdrb200@naver.com>
2021-10-13 18:25:01 +0900

    ..

--- src/main/java/com/example/dreampicturespring/repository/PaintingRepository.java
+++ src/main/java/com/example/dreampicturespring/repository/PaintingRepository.java
@@ -1,18 +1,9 @@
 package com.example.dreampicturespring.repository;
-
 import com.example.dreampicturespring.entity.Paintingtbl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import java.util.List;
-
-import java.util.List;
-
 public interface PaintingRepository extends JpaRepository<Paintingtbl,Long> {
-
-
     @Query(value = "select membershiptbl.img, membershiptbl.nickname, paintingtbl.pname from paintingtbl, membershiptbl where paintingtbl.no_membership = membershiptbl.no_membership",nativeQuery = true)
     List<String> findAllPainting_Desc();
-
 }
